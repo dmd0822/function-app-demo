3da4b1e57bac469ace7905c02bf15687516e3cbe
Dave Davis <dave@davemdavis.net>
2025-06-03 13:13:53 -0400

    Set application insights instrumentation key output as sensitive

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,7 +5,8 @@ output "app_service_plan_id" {
   value = module.app_service_plan.app_service_plan_id
 }
 output "application_insights_instrumentation_key" {
-  value = module.application_insights.instrumentation_key
+  value     = module.application_insights.instrumentation_key
+  sensitive = true
 }
 output "key_vault_id" {
   value = module.key_vault.key_vault_id

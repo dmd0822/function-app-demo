e6498431a0c9281a2293e674fa77fb1d86bba0c0
Dave Davis <dave@davemdavis.net>
2025-06-03 14:48:37 -0400

    Update Terraform backend configuration and modify key vault name in dev.tfvars

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,12 @@ provider "azurerm" {
 }
 
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-func-demo"
+    storage_account_name = "safunctfstate"
+    container_name       = "tfstatecontainer"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"

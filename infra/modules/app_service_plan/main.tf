cc2b17a74149cf5d4830e817e6e69fd43dd5048c
Dave Davis <dave@davemdavis.net>
2025-06-03 15:28:39 -0400

    Update SKU name for Azure Service Plan to Elastic Premium

--- infra/modules/app_service_plan/main.tf
+++ infra/modules/app_service_plan/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_service_plan" "asp" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "EP1" # Elastic Premium
 }

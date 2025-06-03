676b1f0a826cc9c91af216d45eeb0bf74e78c4fd
Dave Davis <dave@davemdavis.net>
2025-06-03 12:11:27 -0400

    Remove temporary backend configuration from Terraform main.tf and ensure ARM_SUBSCRIPTION_ID is set in the workflow environment.

--- infra/main.tf
+++ infra/main.tf
@@ -9,12 +9,6 @@ terraform {
       version = ">= 3.0.0"
     }
   } 
-  backend "azurerm" {
-    storage_account_name = "temp"
-    container_name       = "temp"
-    key                 = "terraform.tfstate"
-  }
-
 }
 
 data "azurerm_resource_group" "main" {

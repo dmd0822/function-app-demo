4e367d9177848d6eb2ba6ac0a2f41b34787f52d9
Dave Davis <dave@davemdavis.net>
2025-06-24 10:31:12 -0400

    Add MonkeyFunction class with GetMonkey HTTP trigger for retrieving monkeys

---
+++ src/monkey/MonkeyFunction.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Monkey.Services;
+using System.Threading.Tasks;
+
+namespace Monkey
+{
+    public class MonkeyFunction
+    {
+        private readonly ILogger<MonkeyFunction> _logger;
+
+        public MonkeyFunction(ILogger<MonkeyFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetMonkey")]
+        public async Task<HttpResponseData> Run([
+            HttpTrigger(AuthorizationLevel.Function, "get", "post")]
+            HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var monkeyService = new MonkeyService();
+            var monkeys = await monkeyService.GetMonkeys();
+            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+            await response.WriteStringAsync(JsonSerializer.Serialize(monkeys));
+            return response;
+        }
+    }
+}
